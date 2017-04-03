b1bcab9f147ea366c7a360ea3ae9e5b34fd60f39
Michal Policht <michpolicht@gmail.com>
2017-04-03 11:09:26 +0200

    Remove classes moved to "LibServices".

--- QML/CuteHMI/Base/src/CuteHMIBaseQMLPlugin.cpp
+++ QML/CuteHMI/Base/src/CuteHMIBaseQMLPlugin.cpp
@@ -1,25 +1,22 @@
 #include "CuteHMIBaseQMLPlugin.hpp"
 
-#include <base/ServiceRegistry.hpp>
+#include <base/CuteHMI.hpp>
 
 #include <QtQml>
 
 void CuteHMIBaseQMLPlugin::registerTypes(const char * uri)
 {
 	Q_ASSERT(uri == QLatin1String("CuteHMI.Base"));
-
-	qmlRegisterType<cutehmi::base::Service>(uri, 1, 0, "Service");
-	qmlRegisterSingletonType<cutehmi::base::ServiceRegistry>(uri, 1, 0, "ServiceRegistry", ServiceRegistryProvider);
 }
 
-QObject * CuteHMIBaseQMLPlugin::ServiceRegistryProvider(QQmlEngine * engine, QJSEngine * scriptEngine)
-{
-	Q_UNUSED(scriptEngine)
+//QObject * CuteHMIBaseQMLPlugin::ServiceRegistryProvider(QQmlEngine * engine, QJSEngine * scriptEngine)
+//{
+//	Q_UNUSED(scriptEngine)
 
-	cutehmi::base::ServiceRegistry * serviceRegistry = & cutehmi::base::ServiceRegistry::Instance();
-	engine->setObjectOwnership(serviceRegistry, QQmlEngine::CppOwnership);
-	return serviceRegistry;
-}
+//	cutehmi::base::ServiceRegistry * serviceRegistry = cutehmi::base::CuteHMI::Instance().project()->serviceRegistry();
+//	engine->setObjectOwnership(serviceRegistry, QQmlEngine::CppOwnership);
+//	return serviceRegistry;
+//}
 
 //(c)MP: Copyright © 2017, Michal Policht. All rights reserved.
 //(c)MP: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
