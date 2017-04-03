f1abdd6de4f099703466523e27020ffa2c24ed83
Michal Policht <michpolicht@gmail.com>
2017-04-03 11:00:16 +0200

    Implement "PluginServices".

---
+++ PluginServices/src/services/plugin/Plugin.hpp
@@ -0,0 +1,35 @@
+#ifndef CUTEHMI_PLUGINSERVICES_SRC_SERVICES_PLUGIN_PLUGIN_HPP
+#define CUTEHMI_PLUGINSERVICES_SRC_SERVICES_PLUGIN_PLUGIN_HPP
+
+#include <base/IPlugin.hpp>
+
+#include <QObject>
+
+#include <memory>
+
+namespace cutehmi {
+namespace services {
+namespace plugin {
+
+class Plugin:
+	public QObject,
+	public base::IPlugin
+{
+	Q_OBJECT
+	Q_PLUGIN_METADATA(IID CUTEHMI_BASE_IPLUGIN_IID FILE "PluginServices.json")
+	Q_INTERFACES(cutehmi::base::IPlugin)
+
+	public:
+		static constexpr const char * NAMESPACE_URI = "http://services.cutehmi";
+
+		void init(base::ProjectNode & node) override;
+};
+
+}
+}
+}
+
+#endif
+
+//(c)MP: Copyright © 2017, Michal Policht. All rights reserved.
+//(c)MP: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
---
+++ PluginServices/src/services/plugin/PluginNodeData.hpp
@@ -0,0 +1,31 @@
+#ifndef CUTEHMI_PLUGINSERVICES_SRC_SERVICES_PLUGIN_PLUGINNODEDATA_HPP
+#define CUTEHMI_PLUGINSERVICES_SRC_SERVICES_PLUGIN_PLUGINNODEDATA_HPP
+
+#include <services/ServiceRegistry.hpp>
+
+#include <base/DataBlock.hpp>
+
+namespace cutehmi {
+namespace services {
+namespace plugin {
+
+class PluginNodeData:
+	public base::DataBlock
+{
+	public:
+		PluginNodeData();
+
+		ServiceRegistry * serviceRegistry() const;
+
+	private:
+		std::unique_ptr<ServiceRegistry> m_serviceRegistry;
+};
+
+}
+}
+}
+
+#endif
+
+//(c)MP: Copyright © 2017, Michal Policht. All rights reserved.
+//(c)MP: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
