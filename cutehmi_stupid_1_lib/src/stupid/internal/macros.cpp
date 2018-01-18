a0ece894410ea76c4e01bcdf3d61bc2d8ed790cf
Michal Policht <michpolicht@gmail.com>
2018-01-18 15:44:25 +0100

    Fix logging category name.

--- cutehmi_stupid_1_lib/src/stupid/internal/macros.cpp
+++ cutehmi_stupid_1_lib/src/stupid/internal/macros.cpp
@@ -1,6 +1,6 @@
 #include "../../../include/stupid/internal/macros.hpp"
 
-Q_LOGGING_CATEGORY(cutehmi_stupid_0_lib_loggingCategory, "cutehmi.stupid.0.lib")
+Q_LOGGING_CATEGORY(cutehmi_stupid_1_lib_loggingCategory, "cutehmi_stupid_1_lib")
 
 //(c)MP: Copyright © 2017, Michal Policht. All rights reserved.
 //(c)MP: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
