02282181b014009a86379d69b9cbe5db5ce0c880
Michal Policht <michpolicht@gmail.com>
2018-07-10 18:49:38 +0200

    Update license and include guards.

--- modules/cutehmi_xml_1/src/cutehmi/xml/plugin/MetadataPlugin.hpp
+++ modules/cutehmi_xml_1/src/cutehmi/xml/plugin/MetadataPlugin.hpp
@@ -1,5 +1,5 @@
-#ifndef H_MODULES_CUTEHMI__1_SRC_CUTEHMI_XML_PLUGIN_METADATAPLUGIN_HPP
-#define H_MODULES_CUTEHMI__1_SRC_CUTEHMI_XML_PLUGIN_METADATAPLUGIN_HPP
+#ifndef H_MODULES_CUTEHMI__XML__1_SRC_CUTEHMI_XML_PLUGIN_METADATAPLUGIN_HPP
+#define H_MODULES_CUTEHMI__XML__1_SRC_CUTEHMI_XML_PLUGIN_METADATAPLUGIN_HPP
 
 #include "../../../../cutehmi.metadata.hpp"
 
@@ -25,5 +25,5 @@ class MetadataPlugin:
 
 #endif
 
-//(c)MP: Copyright Â© 2018, Michal Policht. All rights reserved.
+//(c)MP: Copyright © 2018, Michal Policht. All rights reserved.
 //(c)MP: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
