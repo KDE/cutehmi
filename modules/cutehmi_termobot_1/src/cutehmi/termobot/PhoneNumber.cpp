522be278fd27977aecebd185da6b546c7f715914
Wojtek Zygmuntowicz <wojtek@wzygmuntowicz.zygmuntowicz@gmail.com>
2018-07-27 18:32:27 +0200

    Delete PhoneNumber class.

--- modules/cutehmi_termobot_1/src/cutehmi/termobot/PhoneNumber.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../../include/cutehmi/termobot/PhoneNumber.hpp"
-
-namespace cutehmi {
-namespace termobot {
-
-PhoneNumber::PhoneNumber() {}
-
-} // namespace termobot
-} // namespace cutehmi
