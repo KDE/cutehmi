75105619e860b3c008a369bb6fd9f384486e623d
CuteBOT <michpolicht@gmail.com>
2018-09-15 03:01:39 +0200

    Update license.

--- modules/cutehmi_test_1/include/cutehmi/test/random.hpp
+++ modules/cutehmi_test_1/include/cutehmi/test/random.hpp
@@ -74,3 +74,6 @@ typename std::enable_if<std::is_integral<T>::value, T>::type rand(T from = std::
 }
 
 #endif
+
+//(c)MP: Copyright © 2018, Michal Policht. All rights reserved.
+//(c)MP: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
