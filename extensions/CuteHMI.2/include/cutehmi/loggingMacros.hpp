699c7e90ac0e9562436aca210936c791634902d0
Michal Policht <michal@policht.pl>
2018-12-12 12:06:07 +0100

    Update logging macros.

--- extensions/CuteHMI.2/include/cutehmi/loggingMacros.hpp
+++ extensions/CuteHMI.2/include/cutehmi/loggingMacros.hpp
@@ -1,33 +1,40 @@
-#ifndef H_MODULES_CUTEHMI__1_INCLUDE_CUTEHMI_LOGGINGMACROS_HPP
-#define H_MODULES_CUTEHMI__1_INCLUDE_CUTEHMI_LOGGINGMACROS_HPP
+#ifndef H_MODULES_CUTEHMI_u_1_INCLUDE_CUTEHMI_LOGGINGMACROS_HPP
+#define H_MODULES_CUTEHMI_u_1_INCLUDE_CUTEHMI_LOGGINGMACROS_HPP
 
 #include <QLoggingCategory>
 
-#define CUTEHMI_FLF " [file: " << __FILE__ << " line: " << __LINE__ << " function: " << __FUNCTION__ << "]"
-
 #ifndef CUTEHMI_NDEBUG
-	#define CUTEHMI_DEBUG(EXPR) qCDebug(loggingCategory()).nospace().noquote() << "[debug] " << EXPR << CUTEHMI_FLF
+	#define CUTEHMI_DEBUG(EXPR) qCDebug(loggingCategory()).nospace().noquote() << EXPR
 #else
 	#define CUTEHMI_DEBUG(EXPR) (void)0
 #endif
 
 #ifndef CUTEHMI_NINFO
-	#define CUTEHMI_INFO(EXPR) qCInfo(loggingCategory()).nospace().noquote() << "[note] " << EXPR << CUTEHMI_FLF
+	#define CUTEHMI_INFO(EXPR) qCInfo(loggingCategory()).nospace().noquote() << EXPR
 #else
 	#define CUTEHMI_INFO(EXPR) (void)0
 #endif
 
 #ifndef CUTEHMI_NWARNING
-	#define CUTEHMI_WARNING(EXPR) qCWarning(loggingCategory()).nospace().noquote() << "[warning] " << EXPR << CUTEHMI_FLF
+	#define CUTEHMI_WARNING(EXPR) qCWarning(loggingCategory()).nospace().noquote() << EXPR
 #else
 	#define CUTEHMI_WARNING(EXPR) (void)0
 #endif
 
 #ifndef CUTEHMI_NCRITICAL
-	#define CUTEHMI_CRITICAL(EXPR) qCCritical(loggingCategory()).nospace().noquote() << "[critical] " << EXPR << CUTEHMI_FLF
+	#define CUTEHMI_CRITICAL(EXPR) qCCritical(loggingCategory()).nospace().noquote() << EXPR
 #else
 	#define CUTEHMI_CRITICAL(EXPR) (void)0
 #endif
+
+// Changes order of arguments so that CUTEHMI_DIE can be used with single or multiple arguments (__VA_ARGS__ can not be empty).
+template<typename ...ARGS>
+void cutehmiFatalWrapper(const char * category, const char * message, ARGS... args)
+{
+	qFatal(message, category, args...);
+}
+
+#define CUTEHMI_DIE(...) cutehmiFatalWrapper(loggingCategory().categoryName(), "%s [fatal]: " __VA_ARGS__)
 
 #ifndef CUTEHMI_NDEBUG
 	#define CUTEHMI_ASSERT(EXPR, MSG) Q_ASSERT_X(EXPR, __FILE__, MSG)
