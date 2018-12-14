ca8713cc128e950c4b81b0146984dc4b4b2a948c
CuteBOT <michpolicht@gmail.com>
2018-12-14 03:01:14 +0100

    Update license.

--- extensions/CuteHMI.2/src/cutehmi/tst/tst_ExceptionMixin.cpp
+++ extensions/CuteHMI.2/src/cutehmi/tst/tst_ExceptionMixin.cpp
@@ -66,3 +66,6 @@ void tst_ExceptionMixin::concurrent()
 
 QTEST_MAIN(cutehmi::tst_ExceptionMixin)
 #include "tst_ExceptionMixin.moc"
+
+//(c)MP: Copyright © 2018, Michal Policht. All rights reserved.
+//(c)MP: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
