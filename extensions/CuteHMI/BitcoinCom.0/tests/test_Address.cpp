f4a672e298738e69d8524d2df556e2df3cb5d517
CuteBOT <michpolicht@gmail.com>
2019-01-03 03:00:51 +0100

    Update license.

--- extensions/CuteHMI/BitcoinCom.0/tests/test_Address.cpp
+++ extensions/CuteHMI/BitcoinCom.0/tests/test_Address.cpp
@@ -143,5 +143,5 @@ void test_Address::update()
 QTEST_MAIN(cutehmi::bitcoincom::test_Address)
 #include "test_Address.moc"
 
-//(c)MP: Copyright © 2018, Michal Policht. All rights reserved.
+//(c)MP: Copyright © 2019, Michal Policht. All rights reserved.
 //(c)MP: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
--- extensions/CuteHMI/Test.0/tests/test_random.cpp
+++ extensions/CuteHMI/Test.0/tests/test_random.cpp
@@ -26,5 +26,5 @@ void test_random::rand()
 QTEST_MAIN(cutehmi::test::test_random)
 #include "test_random.moc"
 
-//(c)MP: Copyright © 2018, Michal Policht. All rights reserved.
+//(c)MP: Copyright © 2019, Michal Policht. All rights reserved.
 //(c)MP: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
