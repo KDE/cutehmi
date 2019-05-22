de07d3a4b2cb730ac10cea77bc776ece95acd37a
Wojtek Zygmuntowicz <wzygmuntowicz.zygmuntowicz@gmail.com>
2019-05-22 23:56:03 +0200

    Fix formating of termobot module.

--- modules/cutehmi_termobot_1/include/cutehmi/termobot/DS18B20SettingsModel.hpp
+++ modules/cutehmi_termobot_1/include/cutehmi/termobot/DS18B20SettingsModel.hpp
@@ -57,7 +57,7 @@ class CUTEHMI_TERMOBOT_API DS18B20SettingsModel:
 
 		Q_INVOKABLE QVariant descriptionFromW1Id(const QString & w1Id) const;
 
-                Q_INVOKABLE QModelIndex indexFromW1Id(const QString & w1Id) const;
+		Q_INVOKABLE QModelIndex indexFromW1Id(const QString & w1Id) const;
 
 		Q_INVOKABLE int roleId(const QByteArray & name) const;
 
@@ -185,5 +185,5 @@ class CUTEHMI_TERMOBOT_API DS18B20SettingsModel:
 
 #endif
 
-//(c)WZ: Copyright © 2018, Wojciech Zygmuntowicz. All rights reserved.
+//(c)WZ: Copyright © 2019, Wojciech Zygmuntowicz. All rights reserved.
 //(c)WZ: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
