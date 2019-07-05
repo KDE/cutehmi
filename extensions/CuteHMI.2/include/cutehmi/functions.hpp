3f8af30aa9d3f68170ad5a919453db1f34ab65a8
Michal Policht <michpolicht@gmail.com>
2019-07-05 19:45:55 +0200

    Implement functions for floating point numbers comparison.

---
+++ extensions/CuteHMI.2/include/cutehmi/functions.hpp
@@ -0,0 +1,130 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+#include "constants.hpp"
+
+namespace cutehmi {
+
+/**
+ * Epsilon for a number. Get epsilon for real number @a r, that means smallest number which added to @a r changes value of @a r.
+ * @param r real number.
+ * @return smallest number, which added to @a r changes value of @a r. If @a r is positive number, then returned number is also
+ * positive. Otherwise negative number is returned.
+ *
+ * @see absEps().
+ */
+inline
+qreal eps(qreal r)
+{
+	static constexpr qreal MACHINE_EPS = std::numeric_limits<qreal>::epsilon() * 0.5;
+
+	int exp;
+	std::frexp(r, & exp);
+	return std::ldexp(std::signbit(r) ? -MACHINE_EPS : MACHINE_EPS, exp);
+}
+
+/**
+ * Absolute epsilon for a number. Work in similar way to eps() function, except that absolute value is returned instead of signed one.
+ * @param r real number.
+ * @return smallest number which added to @a r changes value of @a r. Always non-negative value is returned.
+ *
+ * @see eps().
+ */
+inline
+qreal absEps(qreal r)
+{
+	static constexpr qreal MACHINE_EPS = std::numeric_limits<qreal>::epsilon() * 0.5;
+
+	int exp;
+	std::frexp(r, & exp);
+	return std::ldexp(MACHINE_EPS, exp);
+}
+
+/**
+ * Approximately equal. Compares real numbers @a r1, @a r2.
+ * @param r1 first number to compare.
+ * @param r2 second number to compare.
+ * @param eps epsilon tolerance parameter used in comparison.
+ * @return @p true when @a r1 and @a r2 are approximately equal, @p false otherwise.
+ *
+ * @see ale().
+ *
+ * @internal Donald E. Knuth "Art of Computer Programming" [P 4.2.2 Vol2].
+ */
+inline
+bool ape(qreal r1, qreal r2, qreal eps = EPS)
+{
+	int exp1, exp2;
+	std::frexp(r1, & exp1);
+	std::frexp(r2, & exp2);
+
+	// For some reason <= is used in the book to compare both sides. There may be a reason for this, so I'll leave it for now, but
+	// the consequence is that image of a function is a bit weird, e.g.:
+	// ape(1.0, 1.99, 0.25) -> false, ape(1.0, 2.0, 0.25) -> true, ape(1.0, 2.01, 0.25) -> false
+	return std::abs(r2 - r1) <= std::ldexp(eps, std::max(exp1, exp2));
+}
+
+/**
+ * Almost equal. Compares real numbers @a r1, @a r2. Function similar to ape(), however ale() is slightly stronger than ape() (@a r1
+ * and @a r2 must be slightly closer to each other to manifest equality). This function can be problematic, when one of the values
+ * is exactly zero, because in such case it returns @p true only if second argument is also exactly zero.
+ * @param r1 first number to compare.
+ * @param r2 second number to compare.
+ * @param eps epsilon tolerance parameter used in comparison.
+ * @return @p true when @a r1 and @a r2 are almost equal, @p false otherwise.
+ *
+ * @see ape().
+ *
+ * @internal Donald E. Knuth "Art of Computer Programming" [P 4.2.2 Vol2].
+ */
+inline
+bool ale(qreal r1, qreal r2, qreal eps = EPS)
+{
+	int exp1, exp2;
+	std::frexp(r1, & exp1);
+	std::frexp(r2, & exp2);
+
+	return std::abs(r2 - r1) <= std::ldexp(eps, std::min(exp1, exp2));
+}
+
+/**
+ * Considerably less than.
+ * @param r1 first number to compare.
+ * @param r2 second number to compare.
+ * @param eps epsilon tolerance parameter used in comparison.
+ * @return @p true when @a r1 is considerably less than @a r2, @p false otherwise.
+ *
+ * @internal Donald E. Knuth "Art of Computer Programming" [P 4.2.2 Vol2].
+ */
+inline
+bool clt(qreal r1, qreal r2, qreal eps = EPS)
+{
+	int exp1, exp2;
+	std::frexp(r1, & exp1);
+	std::frexp(r2, & exp2);
+
+	return r2 - r1 > std::ldexp(eps, std::max(exp1, exp2));
+}
+
+/**
+ * Considerably greater than.
+ * @param r1 first number to compare.
+ * @param r2 second number to compare.
+ * @param eps epsilon tolerance parameter used in comparison.
+ * @return @p true when @a r1 is considerably greater than @a r2, @p false otherwise.
+ *
+ * @internal Donald E. Knuth "Art of Computer Programming" [P 4.2.2 Vol2].
+ */
+inline
+bool cgt(qreal r1, qreal r2, qreal eps = EPS)
+{
+	int exp1, exp2;
+	std::frexp(r1, & exp1);
+	std::frexp(r2, & exp2);
+
+	return r1 - r2 > std::ldexp(eps, std::max(exp1, exp2));
+}
+
+}
+
+#endif // FUNCTIONS_HPP
---
+++ extensions/CuteHMI.2/tests/test_functions.cpp
@@ -0,0 +1,143 @@
+#include <cutehmi/functions.hpp>
+
+#include <cutehmi/test/random.hpp>
+
+#include <QtTest/QtTest>
+
+namespace cutehmi {
+
+class test_functions:
+	public QObject
+{
+	Q_OBJECT
+
+	private slots:
+		void testApe();
+
+		void testAle();
+
+		void testClt();
+
+		void testCgt();
+
+		void testEps();
+
+		void testAbsEps();
+};
+
+void test_functions::testApe()
+{
+	QCOMPARE(ape(0.0, 1.0e-100, 0.25), true);
+
+	QCOMPARE(ape(0.0, 0.24, 0.25), true);
+	QCOMPARE(ape(0.0, 0.25, 0.25), true);
+	QCOMPARE(ape(0.0, 0.26, 0.25), false);
+
+	QCOMPARE(ape(1.0, 1.49, 0.25), true);
+	QCOMPARE(ape(1.0, 1.51, 0.25), false);
+	QCOMPARE(ape(1.0, 1.99, 0.25), false);
+	QCOMPARE(ape(1.0, 2.0, 0.25), true);	// Weirdness comming from the fact that <= is used in comparison.
+	QCOMPARE(ape(1.0, 2.01, 0.25), false);
+
+	QCOMPARE(ape(1.01, 1.5, 0.25), true);
+	QCOMPARE(ape(1.01, 1.52, 0.25), false);
+	QCOMPARE(ape(1.01, 1.99, 0.25), false);
+	QCOMPARE(ape(1.01, 2.0, 0.25), true);
+	QCOMPARE(ape(1.01, 2.01, 0.25), true);
+
+	QCOMPARE(ape(0.0, 0.0), true);
+	QCOMPARE(ape(0.5, 0.5), true);
+	QCOMPARE(ape(0.5e16, 0.5e16), true);
+	QCOMPARE(ape(0.0, 0.0625), false);
+	QCOMPARE(ape(0.5, 0.5 + 0.0625), false);
+
+	qreal r = test::rand<qreal>(0, std::numeric_limits<qreal>::max_exponent);
+	QCOMPARE(ape(r, r + std::numeric_limits<qreal>::epsilon() * 0.5), true);
+	QCOMPARE(ape(r, 1.0625 * r), false);
+}
+
+void test_functions::testAle()
+{
+	QCOMPARE(ale(0.0, 1.0e-100, 0.25), false);	// Problematic behavior of ale() function.
+
+	QCOMPARE(ale(1.0, 1.125, 0.25), true);
+	QCOMPARE(ale(1.0, 1.99, 0.25), false);
+	QCOMPARE(ale(1.0, 2.0, 0.25), false);
+	QCOMPARE(ale(1.0, 2.01, 0.25), false);
+	QCOMPARE(ale(2.0, 2.01, 0.25), true);
+}
+
+void test_functions::testClt()
+{
+	QCOMPARE(clt(0.0, 0.0, 0.25), false);
+	QCOMPARE(clt(-0.125, 0.0, 0.25), false);
+	QCOMPARE(clt(-0.5, 0.0, 0.25), true);
+
+	QCOMPARE(clt(-1000, -100, 0.25), true);
+	QCOMPARE(clt(-1000, -999, 0.25), false);
+
+	QCOMPARE(clt(1000, 2000, 0.25), true);
+	QCOMPARE(clt(1000, 1001, 0.25), false);
+}
+
+void test_functions::testCgt()
+{
+	QCOMPARE(cgt(0.0, 0.0, 0.25), false);
+	QCOMPARE(cgt(0.0, -0.125, 0.25), false);
+	QCOMPARE(cgt(0.0, -0.5, 0.25), true);
+
+	QCOMPARE(cgt(-100, -1000, 0.25), true);
+	QCOMPARE(cgt(-999, -1000, 0.25), false);
+
+	QCOMPARE(cgt(2000, 1000, 0.25), true);
+	QCOMPARE(cgt(1001, 1000, 0.25), false);
+}
+
+void test_functions::testEps()
+{
+	QVERIFY(eps(1.0) > 0.0);
+	QVERIFY(eps(-1.0) < 0.0);
+
+	QCOMPARE(1.0 == 1.0 + eps(1.0), false);
+	QCOMPARE(1.0 == 1.0 + eps(1.0) * 0.5, true);
+
+	QCOMPARE(2.0 == 2.0 + eps(2.0), false);
+	QCOMPARE(2.0 == 2.0 + eps(2.0) * 0.5, true);
+
+	qreal r = test::rand<qreal>();
+	if (r >= 0)
+		QVERIFY(eps(r) > 0);
+	else
+		QVERIFY(eps(r) < 0);
+	QCOMPARE(r == r + eps(r), false);
+	QCOMPARE(r == r + eps(r) * 0.25, true);
+}
+
+void test_functions::testAbsEps()
+{
+	QVERIFY(absEps(1.0) > 0.0);
+	QVERIFY(absEps(-1.0) > 0.0);
+
+	QCOMPARE(1.0 == 1.0 + absEps(1.0), false);
+	QCOMPARE(1.0 == 1.0 + absEps(1.0) * 0.5, true);
+
+	QCOMPARE(2.0 == 2.0 + absEps(2.0), false);
+	QCOMPARE(2.0 == 2.0 + absEps(2.0) * 0.5, true);
+
+	qreal r = test::rand<qreal>();
+	QVERIFY(absEps(r) > 0);
+	QCOMPARE(r == r + absEps(r), false);
+	QCOMPARE(r == r + absEps(r) * 0.25, true);
+}
+
+
+}
+
+QTEST_MAIN(cutehmi::test_functions)
+#include "test_functions.moc"
+
+//(c)MP: Copyright © 2019, Michal Policht <michpolicht@gmail.com>. All rights reserved.
+//(c)MP: This file is a part of CuteHMI.
+//(c)MP: CuteHMI is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//(c)MP: CuteHMI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+//(c)MP: You should have received a copy of the GNU Lesser General Public License along with CuteHMI.  If not, see <https://www.gnu.org/licenses/>.
