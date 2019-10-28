184d0fc0d9328dc58a364add013fb645ddc0ad45
Mr CuteBOT <michpolicht@gmail.com>
2019-10-28 03:01:25 +0100

    Update license.

--- extensions/CuteHMI/Services.2/include/cutehmi/services/internal/QMLPlugin.cpp
+++ extensions/CuteHMI/Services.2/include/cutehmi/services/internal/QMLPlugin.cpp
@@ -32,7 +32,7 @@ QObject * QMLPlugin::ServiceManagerProvider(QQmlEngine * engine, QJSEngine * scr
 }
 }
 
-//(c)C: Copyright © 2019, Michal Policht <michpolicht@gmail.com>. All rights reserved.
+//(c)C: Copyright © 2019, . All rights reserved.
 //(c)C: This file is a part of CuteHMI.
 //(c)C: CuteHMI is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 //(c)C: CuteHMI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
