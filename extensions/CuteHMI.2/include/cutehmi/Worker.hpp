d6ef8e7f3303786332b009664bd9320ed1b32c2d
Mr CuteBOT <michpolicht@gmail.com>
2019-10-28 13:17:52 +0100

    Update license.

--- extensions/CuteHMI.2/include/cutehmi/Worker.hpp
+++ extensions/CuteHMI.2/include/cutehmi/Worker.hpp
@@ -166,7 +166,7 @@ class CUTEHMI_API Worker:
 
 #endif
 
-//(c)C: Copyright © 2018-2019, . All rights reserved.
+//(c)C: Copyright © 2018-2019, CuteBOT <michpolicht@gmail.com>, Michal Policht <michpolicht@gmail.com>, Mr CuteBOT <michpolicht@gmail.com>. All rights reserved.
 //(c)C: This file is a part of CuteHMI.
 //(c)C: CuteHMI is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 //(c)C: CuteHMI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
