7c7fed2143b2ce7184b096546b26ddf6f42beb67
Mr CuteBOT <michpolicht@gmail.com>
2019-11-09 03:01:52 +0100

    Update license.

--- extensions/CuteHMI/Modbus.2/include/cutehmi/modbus/HoldingRegisterController.hpp
+++ extensions/CuteHMI/Modbus.2/include/cutehmi/modbus/HoldingRegisterController.hpp
@@ -34,7 +34,7 @@ class CUTEHMI_MODBUS_API HoldingRegisterController:
 
 #endif
 
-//(c)C: Copyright © 2019, Mr CuteBOT <michpolicht@gmail.com>, Michal Policht <michpolicht@gmail.com>. All rights reserved.
+//(c)C: Copyright © 2019, Michał Policht <michal@policht.pl>. All rights reserved.
 //(c)C: This file is a part of CuteHMI.
 //(c)C: CuteHMI is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 //(c)C: CuteHMI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
