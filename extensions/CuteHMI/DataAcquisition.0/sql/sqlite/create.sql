295777e5c2fa953fe29b60b38c85edf340b04dfc
Michał Policht <michal@policht.pl>
2020-08-10 17:37:59 +0200

    Fix 'id' row type.

--- extensions/CuteHMI/DataAcquisition.0/sql/sqlite/create.sql
+++ extensions/CuteHMI/DataAcquisition.0/sql/sqlite/create.sql
@@ -29,7 +29,7 @@ CREATE TABLE [%1.recency_real]
 
 CREATE TABLE [%1.event_bool]
 (
-        id serial PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         tag_id INTEGER REFERENCES [%1.tag](id),
         value BOOL NOT NULL,
         time INTEGER NOT NULL
@@ -39,7 +39,7 @@ CREATE INDEX [%1.index_event_bool_time] ON [%1.event_bool] (time);
 
 CREATE TABLE [%1.event_int]
 (
-        id serial PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         tag_id INTEGER REFERENCES [%1.tag](id),
         value INTEGER NOT NULL,
         time INTEGER NOT NULL
@@ -49,7 +49,7 @@ CREATE INDEX [%1.index_event_int_time] ON [%1.event_int] (time);
 
 CREATE TABLE [%1.event_real]
 (
-        id serial PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         tag_id INTEGER REFERENCES [%1.tag](id),
         value double precision NOT NULL,
         time INTEGER NOT NULL
@@ -59,7 +59,7 @@ CREATE INDEX [%1.index_event_real_time] ON [%1.event_real] (time);
 
 CREATE TABLE [%1.history_bool]
 (
-        id serial PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         tag_id INTEGER REFERENCES [%1.tag](id),
         open BOOL NOT NULL,
         close BOOL NOT NULL,
@@ -74,7 +74,7 @@ CREATE INDEX [%1.index_history_bool_close_time] ON [%1.history_bool] (close_time
 
 CREATE TABLE [%1.history_int]
 (
-        id serial PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         tag_id INTEGER REFERENCES [%1.tag](id),
         open INTEGER NOT NULL,
         close INTEGER NOT NULL,
@@ -89,7 +89,7 @@ CREATE INDEX [%1.index_history_int_close_time] ON [%1.history_int] (close_time);
 
 CREATE TABLE [%1.history_real]
 (
-        id serial PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         tag_id INTEGER REFERENCES [%1.tag](id),
         open double precision NOT NULL,
         close double precision NOT NULL,
