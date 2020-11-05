8e0025364edeb704e98e342d38bb9914fd875294
Michał Policht <michal@policht.pl>
2020-11-05 15:04:54 +0100

    Provide world-compatible file names in generated 'Messages.sh'

--- awkgward/generate_messages.sh
+++ awkgward/generate_messages.sh
@@ -3,7 +3,7 @@
 # Script that generates Messages.sh, which is a part of KDE translation process.
 
 separator="-"
-prefix="cutehmi"$separator
+prefix=""
 suffix="_qt"
 subdir=""
 tag_file="l10n-kf5.tag"
@@ -41,16 +41,16 @@ if [ $# -gt 4 ]; then
 	separator=$5
 fi
 
-extensions=`find $cutehmi_dir/$subdir -name $tag_file -exec dirname {} \;`
+products=`find $cutehmi_dir/$subdir -name $tag_file -exec dirname {} \;`
 cutehmi_path=`readlink -f $cutehmi_dir`
 messages_path=$cutehmi_path/$messages_file
 
-for extension in $extensions
+for product in $products
 do
-	extension_path=`readlink -f $extension`
+	product_path=`readlink -f $product`
 	# Appending trailing slash to cutehmi_path removes also leading slash.
-	stripped_path=${extension_path#$cutehmi_path\/}
-	translation_file=$prefix`echo $stripped_path | sed 's|[./]|'$separator'|g'`$suffix.pot
+	stripped_path=${product_path#$cutehmi_path\/}
+	translation_file=$prefix`echo ${product_path#$cutehmi_path\/$subdir\/} | sed 's|[./]|'$separator'|g' | awk '{ print tolower($0) }'`$suffix.pot
 	echo \$EXTRACT_TR_STRINGS \`find ./$stripped_path -name \\*.qml -o -name \\*.cpp -o -name \\*.hpp\` -I ./$stripped_path/include -o \$podir/$translation_file
 done
 
