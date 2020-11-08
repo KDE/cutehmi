38a061725e2ec76fbe210d28a836f58647d38cca
Michał Policht <michal@policht.pl>
2020-11-08 22:00:44 +0100

    Update `Messages.sh`

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-$EXTRACT_TR_STRINGS `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/cutehmi_qt.pot
+$EXTRACT_TR_STRINGS `find ./extensions/CuteHMI.2 -name \*.qml -o -name \*.cpp -o -name \*.hpp` -I ./extensions/CuteHMI.2/include -o $podir/cutehmi-2_qt.pot
+$EXTRACT_TR_STRINGS `find ./tools/cutehmi.view.4 -name \*.qml -o -name \*.cpp -o -name \*.hpp` -I ./tools/cutehmi.view.4/include -o $podir/cutehmi-view-4_qt.pot
