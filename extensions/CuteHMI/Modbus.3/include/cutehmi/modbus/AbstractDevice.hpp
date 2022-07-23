3cae64befc162a7a4cb0aa5b3a8af293ce3c4ff1
Mr CuteBOT <michpolicht@gmail.com>
2022-07-23 05:02:00 +0000

    Update license.

--- extensions/CuteHMI/Modbus.3/include/cutehmi/modbus/AbstractDevice.hpp
+++ extensions/CuteHMI/Modbus.3/include/cutehmi/modbus/AbstractDevice.hpp
@@ -710,7 +710,7 @@ Q_DECLARE_METATYPE(cutehmi::modbus::AbstractDevice::State)	// Must use this macr
 
 #endif
 
-//(c)C: Copyright © 2021, Michał Policht <michal@policht.pl>. All rights reserved.
+//(c)C: Copyright © 2021-2022, Michał Policht <michal@policht.pl>. All rights reserved.
 //(c)C: SPDX-License-Identifier: LGPL-3.0-or-later OR MIT
 //(c)C: This file is a part of CuteHMI.
 //(c)C: CuteHMI is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
