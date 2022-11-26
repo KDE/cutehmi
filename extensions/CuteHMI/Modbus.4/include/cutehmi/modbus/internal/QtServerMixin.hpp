aa88bdf72a52f64f0789f611249a8c6bca941dbc
Mr CuteBOT <michpolicht@gmail.com>
2022-11-26 05:03:28 +0000

    Update license.

--- extensions/CuteHMI/Modbus.4/include/cutehmi/modbus/internal/QtServerMixin.hpp
+++ extensions/CuteHMI/Modbus.4/include/cutehmi/modbus/internal/QtServerMixin.hpp
@@ -177,7 +177,7 @@ DERIVED & QtServerMixin<DERIVED>::derived()
 
 #endif
 
-//(c)C: Copyright © 2021, Michał Policht <michal@policht.pl>. All rights reserved.
+//(c)C: Copyright © 2022, Michał Policht <michal@policht.pl>. All rights reserved.
 //(c)C: SPDX-License-Identifier: LGPL-3.0-or-later OR MIT
 //(c)C: This file is a part of CuteHMI.
 //(c)C: CuteHMI is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
--- extensions/CuteHMI/Modbus.4/include/cutehmi/modbus/internal/RegisterTraits.hpp
+++ extensions/CuteHMI/Modbus.4/include/cutehmi/modbus/internal/RegisterTraits.hpp
@@ -46,7 +46,7 @@ struct RegisterTraits<Coil>
 
 #endif
 
-//(c)C: Copyright © 2021, Michał Policht <michal@policht.pl>. All rights reserved.
+//(c)C: Copyright © 2022, Michał Policht <michal@policht.pl>. All rights reserved.
 //(c)C: SPDX-License-Identifier: LGPL-3.0-or-later OR MIT
 //(c)C: This file is a part of CuteHMI.
 //(c)C: CuteHMI is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
